7a3c22da25d4a04ac877dec8ce7de829c841c09a
Thomas Haddad <thomas.af.haddad@gmail.com>
2015-04-13 11:24:30 +0200

    added upload file form, created Image entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,30 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Image;
+use AppBundle\Form\ImageType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/upload-image", name="uploadImage")
+     */
+    public function uploadImageAction(){
+        $image = new Image();
+        $imageForm = $this->createForm( new ImageType(), $image);
+        $params = [
+          'imageForm'=>$imageForm->createView()
+        ];
+        return $this->render('upload/upload-image.html.twig',$params);
     }
 }
